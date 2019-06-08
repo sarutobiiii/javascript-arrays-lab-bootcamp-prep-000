d6a567b921164565714b1457354ea2255140a63f
oluwatobi balogun <tobibags19@gmail.com>
2019-06-08 22:37:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function prepenKitten(name) {
 }
 
 function removeLastKitten(name) {
-  return kittens.slice(-1)
+  return kittens.slice(0, kittens.length - 1)
 }
 
 function removeFirstKitten(name) {
